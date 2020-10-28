4672b690a94c6c0c8dc7f0f954830d5cf8c89681
Uyen-hub <54424033+Uyen-hub@users.noreply.github.com>
2020-10-27 21:05:24 -0500

    Alien animation ok

---
+++ Fishing Trip/Assets/Assets/AnimationControl.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+
+public class AnimationControl: MonoBehaviour
+{
+    
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+
+    void Update()
+    {
+        float moveH = Input.GetAxisRaw("Horizontal");
+        Run(moveH);
+        Jump();
+
+
+    }
+
+    public void Run(float moveH)
+    {
+        if(Mathf.Abs(moveH)>0)
+        {
+            if (moveH > 0)
+                anim.SetInteger("Run", 2);
+
+            if (moveH < 0)
+            {
+                anim.SetInteger("Run", 1);
+            }
+
+        }
+        else
+            
+            anim.SetInteger("Run", 0);
+        
+    }
+   
+    public void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetInteger("Jump", 1);
+        }
+        else
+        {
+            anim.SetInteger("Jump", 0);
+        }
+          
+
+    }
+
+
+
+}
+
+
+
+
+
+
