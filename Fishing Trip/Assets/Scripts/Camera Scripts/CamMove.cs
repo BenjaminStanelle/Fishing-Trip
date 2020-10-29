b8689c43ccd6c8ab7a8bb0d7cf36dc7f9eef606d
EmbersGlow <jeremyejones@mavs.uta.edu>
2020-10-29 18:54:07 -0500

    Scripting D1, Part 2; Jeremy
    Created camera movement and borders

---
+++ Fishing Trip/Assets/Scripts/Camera Scripts/CamMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public PlayerMove MyPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        MyPlayer = FindObjectOfType<PlayerMove>();
+        rb.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(MyPlayer.BaseSpeed, 0);
+    }
+}
---
+++ Fishing Trip/Assets/Scripts/Camera Scripts/CameraBorderObject2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBorderObject2 : MonoBehaviour
+{
+    private Vector2 screenBounds;
+    private Rigidbody2D rb;
+
+    public PlayerMove MyPlayer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        transform.position = new Vector3(screenBounds.x, screenBounds.y, 0);
+        rb = GetComponent<Rigidbody2D>();
+        MyPlayer = FindObjectOfType<PlayerMove>();
+        rb.gravityScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(MyPlayer.BaseSpeed, 0);
+    }
+}
