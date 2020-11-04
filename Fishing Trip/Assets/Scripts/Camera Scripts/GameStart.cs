9f8dc35b965f08c9f557b800c8dfe545439fec10
Tia <51142215+tiabenson@users.noreply.github.com>
2020-11-04 15:58:46 -0600

    Instantiated 1st background
    Also added background prefabs

--- Fishing Trip/Assets/Scripts/Camera Scripts/GameStart.cs
+++ Fishing Trip/Assets/Scripts/Camera Scripts/GameStart.cs
@@ -7,12 +7,25 @@ public class GameStart : MonoBehaviour
     [SerializeField] private Transform Player5 = null;
     [SerializeField] private Transform BorderLeft = null;
     [SerializeField] private Transform BorderRight = null;
+
+        //Tia Benson
+    public Transform field = null;    
+    public Camera cam = null;         
+    private Transform tempField;
+
     // Awake is called before Start
     void Awake()
     {
+        cam = GetComponent<Camera>();   //TB
+
         Instantiate(BorderLeft, new Vector3(0, 0, 0), Quaternion.Euler(0,0,90));
         Instantiate(BorderRight, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 90));
         Instantiate(Player5, new Vector3(0, 0, 0), transform.rotation);
+
+        //Tia Benson
+        Transform Field = Instantiate(field, new Vector3(cam.transform.position.x, cam.transform.position.y, 0), cam.transform.rotation);   
+        tempField = Field.transform.parent;
+        Field.transform.parent = cam.transform;
         //This stuff creates camera borders, and the player. Player creation will be updated in future.
     }
 
