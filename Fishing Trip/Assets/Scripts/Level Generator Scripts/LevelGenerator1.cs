8e9c86e9bfe58cd6e40ff6f115fd1a5afc647fb4
EmbersGlow <jeremyejones@mavs.uta.edu>
2020-11-07 16:01:48 -0600

    Ground spawn work, destruction script work

--- Fishing Trip/Assets/Scripts/Level Generator Scripts/LevelGenerator1.cs
+++ Fishing Trip/Assets/Scripts/Level Generator Scripts/LevelGenerator1.cs
@@ -7,7 +7,7 @@ public class LevelGenerator1 : MonoBehaviour
     [SerializeField] private GameObject groundpart = null; //allow field to be assigned in inspector
     [SerializeField] private GameObject groundstart = null;
 
-    private GameObject Player, lastgroundpart, GroundEnd, temp;
+    private GameObject Player, lastgroundpart, GroundEnd;
     private Vector3 lastEndPosition; //for spawning more ground
 
 
@@ -16,13 +16,13 @@ public class LevelGenerator1 : MonoBehaviour
     // Awake is called before Start
     private void Awake()
     {
-        lastEndPosition = groundstart.transform.Find("EndPosition").position;
+        lastEndPosition = groundstart.transform.Find("EndPosition").position;//Find first end position
     }
 
     // Start is called before the first frame update
     private void Start()
     {
-        Player = GameObject.FindWithTag("Player");
+        Player = GameObject.FindWithTag("Player");//Find player after it has been created
     }
 
     // Update is called once per frame
@@ -30,17 +30,14 @@ public class LevelGenerator1 : MonoBehaviour
     {
         if(Vector3.Distance(Player.transform.position, lastEndPosition) < Spawn_Distance_To_Player)
         {
-            Destroy(temp);  //destroy previous platform
-            SpawnGroundProcess();
+            SpawnGroundProcess();//if far from player, run SpawnGroundProcess
         }
     }
 
     private void SpawnGroundProcess()
     {
-        temp = GroundEnd;   //create temporary variable to hold platform
-
-        lastgroundpart = SpawnGround(lastEndPosition);
-        lastEndPosition = lastgroundpart.transform.Find("EndPosition").position;
+        lastgroundpart = SpawnGround(lastEndPosition); // last ground part is the returned location of the newest spawned ground
+        lastEndPosition = lastgroundpart.transform.Find("EndPosition").position; //last end position is the location of the EndPosition object from the last ground part
     }
 
     private GameObject SpawnGround(Vector3 SpawnPosition)
---
+++ Fishing Trip/Assets/Scripts/Level Generator Scripts/ObjectSelfDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelfDestroy : MonoBehaviour
+{
+    public float waittime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator OnBecameInvisible()
+    {
+        yield return new WaitForSecondsRealtime(waittime);
+        Destroy(this.gameObject);
+    }
+
+
+}
