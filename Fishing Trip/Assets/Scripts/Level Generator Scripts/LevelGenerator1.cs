bafc15b21b1691086b4eb038be5161ccb8d01a35
Tia <51142215+tiabenson@users.noreply.github.com>
2020-11-06 19:41:49 -0600

    deleting ground offscreen complete

--- Fishing Trip/Assets/Scripts/Level Generator Scripts/LevelGenerator1.cs
+++ Fishing Trip/Assets/Scripts/Level Generator Scripts/LevelGenerator1.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class LevelGenerator1 : MonoBehaviour
 {
-    [SerializeField] private Transform groundpart = null; //allow field to be assigned in inspector
-    [SerializeField] private Transform groundstart = null;
+    [SerializeField] private GameObject groundpart = null; //allow field to be assigned in inspector
+    [SerializeField] private GameObject groundstart = null;
 
-    private GameObject Player;
+    private GameObject Player, lastgroundpart, GroundEnd, temp;
     private Vector3 lastEndPosition; //for spawning more ground
+
 
     public float Spawn_Distance_To_Player; //for distance to spawn ground from player
 
     // Awake is called before Start
     private void Awake()
     {
-        lastEndPosition = groundstart.Find("EndPosition").position;
+        lastEndPosition = groundstart.transform.Find("EndPosition").position;
     }
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -28,19 +30,22 @@ public class LevelGenerator1 : MonoBehaviour
     {
         if(Vector3.Distance(Player.transform.position, lastEndPosition) < Spawn_Distance_To_Player)
         {
+            Destroy(temp);  //destroy previous platform
             SpawnGroundProcess();
         }
     }
 
     private void SpawnGroundProcess()
     {
-        Transform lastgroundpart = SpawnGround(lastEndPosition);
-        lastEndPosition = lastgroundpart.Find("EndPosition").position;
+        temp = GroundEnd;   //create temporary variable to hold platform
+
+        lastgroundpart = SpawnGround(lastEndPosition);
+        lastEndPosition = lastgroundpart.transform.Find("EndPosition").position;
     }
 
-    private Transform SpawnGround(Vector3 SpawnPosition)
+    private GameObject SpawnGround(Vector3 SpawnPosition)
     {
-        Transform GroundEnd = Instantiate(groundpart, SpawnPosition, transform.rotation);
+        GroundEnd = Instantiate(groundpart, SpawnPosition, transform.rotation);
         return GroundEnd;
     }
 }
