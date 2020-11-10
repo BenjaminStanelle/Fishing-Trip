f70e61af8c32b8a8ba440bda15666d9e540950f7
BenjaminStan <benjamin.stanelle@mavs.uta.edu>
2020-11-09 19:45:16 -0600

    Dog and bull prefabs, spawner prefab
    Dog and bull prefabs, spawner prefab

---
+++ Fishing Trip/Assets/Scripts/Enemy Scripts/MoveDog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
+
+
+public class MoveDog : MonoBehaviour
+{
+    public ParticleSystem dust;
+    Vector3 movement = new Vector3(1, 0f, 0f);
+    public float speed = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDust();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move the bull left at certain speed
+        transform.Translate(movement * speed * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if its a player kill the player then switch to menu
+        if (other.gameObject.tag == "Player") ;
+        {
+            Destroy(other.gameObject);
+            //UnloadSceneAsync("SampleScene");
+            SceneManager.LoadScene("MainMenu");
+
+        }
+    }
+    void CreateDust()
+    {
+        dust.Play();
+    }
+
+}
---
+++ Fishing Trip/Assets/Scripts/Enemy Spawner/enemySpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    public int IsRunning = 1;
+    public int NumberofSeconds;
+
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+    int bullSpawn = 0;
+    int dogSpawn = 1;
+    int pick = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(IsRunning == 1)
+        {
+            StartCoroutine(Wait());
+        }
+
+    }
+    void spawnBullFun()
+    {
+        //if (Mathf.RoundToInt(Time.time) % 30 == 1)  
+            Instantiate(enemyPrefabs[bullSpawn], spawnPoints[bullSpawn].position, transform.rotation);
+        
+    }
+    void spawnDogFun()
+    {
+        //if (Mathf.RoundToInt(Time.time) % 30 == 1)  
+        Instantiate(enemyPrefabs[dogSpawn], spawnPoints[dogSpawn].position, transform.rotation);
+
+    }
+
+    public IEnumerator Wait()
+    {
+        IsRunning = 0;
+        yield return new WaitForSeconds(NumberofSeconds);
+        pick = Random.Range(0, enemyPrefabs.Length);
+
+        if (pick == 0)
+        {
+            spawnBullFun();
+        }
+        else if (pick == 1)
+        {
+            spawnDogFun();
+        }
+        IsRunning = 1;
+    }
+}
