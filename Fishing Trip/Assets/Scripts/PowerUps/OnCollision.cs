db5894ff348e5db2d9d58729aed16513ee7f2b52
Uyen-hub <54424033+Uyen-hub@users.noreply.github.com>
2020-11-10 18:30:38 -0600

    PowerUps Script and SoundFX done

---
+++ Fishing Trip/Assets/Scripts/PowerUps/OnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollision : MonoBehaviour
+{
+  
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision detach");
+        if (collision.gameObject.tag =="Player")
+        {
+            Destroy(this.gameObject); 
+        }
+      
+
+    }
+    
+
+}
