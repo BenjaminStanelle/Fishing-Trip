b648cda2ff39ad203394bc6b0a6dad4057b31097
Tia <51142215+tiabenson@users.noreply.github.com>
2020-11-10 19:49:52 -0600

    pause button on game scene

---
+++ Fishing Trip/Assets/Scripts/MenuScripts/GameMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Tia Benson
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject gameMenu;
+    [SerializeField] private AudioSource audio = null;
+
+    public void onPause() //when pause button pressed, show pause screen & pause audio
+    {
+        gameMenu.SetActive(true);
+        Time.timeScale = 0f;
+        audio.Pause();
+
+        
+    }
+}
---
+++ Fishing Trip/Assets/Scripts/MenuScripts/PauseScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Tia Benson
+
+public class PauseScreen : MonoBehaviour
+{
+   [SerializeField] private AudioSource audio = null;
+
+    public void onContinue()    //when play button pressed, continue audio and game
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        audio.Play(0);
+    }
+}
