dfa118f13d21a7bd1cf8a2ef8985cbdabf48e376
BenjaminStan <benjamin.stanelle@mavs.uta.edu>
2020-11-11 05:21:57 -0600

    Fixed Bull and Dog
    bull and dog prefabs now only destroy objects tagged as "Player", and despawn after 4 seconds

--- Fishing Trip/Assets/Scripts/Enemy Scripts/MoveBull.cs
+++ Fishing Trip/Assets/Scripts/Enemy Scripts/MoveBull.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityStandardAssets.CrossPlatformInput;
@@ -7,6 +9,9 @@ using UnityEngine.SceneManagement;
 
 public class MoveBull : MonoBehaviour
 {
+    public int NumberofSeconds;
+    public int IsRunning = 1;
+
     public ParticleSystem dust;
     Vector3 movement = new Vector3(-1, 0f, 0f);
     public float speed = 15;
@@ -22,12 +27,16 @@ public class MoveBull : MonoBehaviour
         //Move the bull left at certain speed
         transform.Translate(movement * speed * Time.deltaTime);
         
-    }
+        if(IsRunning == 1)
+            {
+                StartCoroutine(Wait());
+            }
+}
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         //if its a player kill the player then switch to menu
-        if (other.gameObject.tag == "Player") ;
+        if ((other.gameObject.tag == "Player"))
         {
             Destroy(other.gameObject);
             //UnloadSceneAsync("SampleScene");
@@ -38,6 +47,14 @@ public class MoveBull : MonoBehaviour
     void CreateDust()
     {
         dust.Play();
+    }
+
+    public IEnumerator Wait()
+    {
+        IsRunning = 0;
+        yield return new WaitForSeconds(NumberofSeconds);
+        Destroy(this.gameObject);
+        IsRunning = 1;
     }
 
 }
--- Fishing Trip/Assets/Scripts/Enemy Scripts/MoveDog.cs
+++ Fishing Trip/Assets/Scripts/Enemy Scripts/MoveDog.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityStandardAssets.CrossPlatformInput;
@@ -7,6 +9,8 @@ using UnityEngine.SceneManagement;
 
 public class MoveDog : MonoBehaviour
 {
+    public int NumberofSeconds;
+    public int IsRunning = 1;
     public ParticleSystem dust;
     Vector3 movement = new Vector3(1, 0f, 0f);
     public float speed = 15;
@@ -22,12 +26,17 @@ public class MoveDog : MonoBehaviour
         //Move the bull left at certain speed
         transform.Translate(movement * speed * Time.deltaTime);
 
+        if (IsRunning == 1)
+        {
+            StartCoroutine(Wait());
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         //if its a player kill the player then switch to menu
-        if (other.gameObject.tag == "Player") ;
+        if (other.gameObject.tag == "Player")
         {
             Destroy(other.gameObject);
             //UnloadSceneAsync("SampleScene");
@@ -40,4 +49,11 @@ public class MoveDog : MonoBehaviour
         dust.Play();
     }
 
+    public IEnumerator Wait()
+    {
+        IsRunning = 0;
+        yield return new WaitForSeconds(NumberofSeconds);
+        Destroy(this.gameObject);
+        IsRunning = 1;
+    }
 }
