648af3df03dbf8f049f89f87e131890ff1634092
EmbersGlow <jeremyejones@mavs.uta.edu>
2020-11-14 14:26:10 -0600

    Improved coffee script

---
+++ Fishing Trip/Assets/Scripts/Player Scripts/CoffeeBoost.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeBoost : MonoBehaviour
+{
+    public float boostDuration = 5;
+
+    private float boostTimer, OriginalBaseSpeed;
+    private bool boosting;
+    private PlayerMove MyPlayer;
+
+    // Awake is called before Start
+    void Awake()
+    {
+        boostTimer = 0;
+        boosting = false;
+        MyPlayer = FindObjectOfType<PlayerMove>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        OriginalBaseSpeed = MyPlayer.BaseSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (boosting)
+        {
+            boostTimer += Time.deltaTime;
+            if (boostTimer >= boostDuration)
+            {
+                boostTimer = 0;
+                MyPlayer.BaseSpeed = OriginalBaseSpeed;
+                boosting = false;
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "coffee")
+        {
+            boosting = true;
+            MyPlayer.BaseSpeed = OriginalBaseSpeed*2;
+        }
+    }
+}
