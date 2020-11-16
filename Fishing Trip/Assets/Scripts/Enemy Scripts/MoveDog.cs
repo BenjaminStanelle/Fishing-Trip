cbdc3f1ea2baf69164b4ed7d1ccc42ddd9206768
EmbersGlow <jeremyejones@mavs.uta.edu>
2020-11-16 15:10:50 -0600

    Rock Prefab, Fixed Dog

--- Fishing Trip/Assets/Scripts/Enemy Scripts/MoveDog.cs
+++ Fishing Trip/Assets/Scripts/Enemy Scripts/MoveDog.cs
@@ -7,7 +7,7 @@ using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine.SceneManagement;
 
 
-public class MoveDog : MonoBehaviour
+public class MoveDog : MonoBehaviour //rewritten by Jeremy
 {
     public int NumberofSeconds;
     public int IsRunning = 1;
@@ -26,26 +26,12 @@ public class MoveDog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move the bull left at certain speed
+        //Move the dog right at speed based on player movement
         transform.Translate(movement * speed * Time.deltaTime);
-
-        if (IsRunning == 1)
-        {
-            StartCoroutine(Wait());
-        }
-
     }
 
     void CreateDust()
     {
         dust.Play();
-    }
-
-    public IEnumerator Wait()
-    {
-        IsRunning = 0;
-        yield return new WaitForSeconds(NumberofSeconds);
-        Destroy(this.gameObject);
-        IsRunning = 1;
     }
 }
