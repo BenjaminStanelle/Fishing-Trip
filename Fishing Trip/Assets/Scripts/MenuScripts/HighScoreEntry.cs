03c66132eb37216d582e6d2c3f23780327035cec
Alexander-Cole <71194937+Alexander-Cole@users.noreply.github.com>
2020-11-22 15:16:46 -0600

    Base HighScore Script.
    Create script that generates the highscore table from a template.

---
+++ Fishing Trip/Assets/Scripts/MenuScripts/HighScoreEntry.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreEntry : MonoBehaviour
+{
+
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+    void Start()
+    {
+        entryContainer = transform.Find("highscoreContainer");
+        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHeight = 33f;
+        for (int i = 0; i <10; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+        }
+
+    }
+}
