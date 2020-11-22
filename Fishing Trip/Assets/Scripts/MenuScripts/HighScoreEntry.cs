cd6bbabc98c91eba4df541bd49c9f22eaccc8702
Alexander-Cole <71194937+Alexander-Cole@users.noreply.github.com>
2020-11-22 15:55:36 -0600

    Highscores prints places and names, not just copies.

--- Fishing Trip/Assets/Scripts/MenuScripts/HighScoreEntry.cs
+++ Fishing Trip/Assets/Scripts/MenuScripts/HighScoreEntry.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class HighScoreEntry : MonoBehaviour
 {
@@ -22,6 +24,31 @@ public class HighScoreEntry : MonoBehaviour
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
             entryTransform.gameObject.SetActive(true);
+
+            int rank = i + 1;
+            string rankString;
+            switch(rank)
+            {
+                default:
+                    rankString = rank + "TH";
+                    break;
+                case 1: 
+                    rankString = "1ST";
+                    break;
+                case 2:
+                    rankString = "2ND";
+                    break;
+                case 3:
+                    rankString = "3RD";
+                    break;
+            }
+            entryTransform.Find("positionTextTemp").GetComponent<TMPro.TextMeshProUGUI>().text = rankString;
+
+            int score = Random.Range(0, 1000);
+            entryTransform.Find("scoreTextTemp").GetComponent<TMPro.TextMeshProUGUI>().text = score.ToString();
+
+            string name = "AAA";
+            entryTransform.Find("nameTextTemp").GetComponent<TMPro.TextMeshProUGUI>().text = name;
         }
 
     }
