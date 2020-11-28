6993290b7312639c79bb087c20e7d8b3fefebc37
EmbersGlow <jeremyejones@mavs.uta.edu>
2020-11-28 17:20:55 -0600

    Skin Dropdown Functioning

--- Fishing Trip/Assets/Scripts/Camera Scripts/GameStart.cs
+++ Fishing Trip/Assets/Scripts/Camera Scripts/GameStart.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class GameStart : MonoBehaviour
 {
-    [SerializeField] private Transform Player1 = null;
-    [SerializeField] private Transform Player2 = null;
-    [SerializeField] private Transform Player3 = null;
-    [SerializeField] private Transform Player4 = null;
-    [SerializeField] private Transform Player5 = null;
+    [SerializeField] private Transform Male1 = null;
+    [SerializeField] private Transform Male2 = null;
+    [SerializeField] private Transform Female1 = null;
+    [SerializeField] private Transform Female2 = null;
+    [SerializeField] private Transform Alien = null;
     [SerializeField] private Transform Border = null;
     private Vector2 screenBounds;
+    private int skin;
 
     // Awake is called before Start
     void Awake()
     {
+        skin = PlayerPrefs.GetInt("Skin", 0);
+        switch (skin)
+        {
+            case 0:
+                Instantiate(Male1, new Vector3(0, -3, 0), transform.rotation);//create player
+                break;
+            case 1:
+                Instantiate(Male2, new Vector3(0, -3, 0), transform.rotation);//create player
+                break;
+            case 2:
+                Instantiate(Female1, new Vector3(0, -3, 0), transform.rotation);//create player
+                break;
+            case 3:
+                Instantiate(Female2, new Vector3(0, -3, 0), transform.rotation);//create player
+                break;
+            case 4:
+                Instantiate(Alien, new Vector3(0, -3, 0), transform.rotation);//create player
+                break;
+        }
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));//find the screenboundaries and save the location
         Instantiate(Border, new Vector3(screenBounds.x * -1, 0, 0), Quaternion.Euler(0,0,90));//place border at left screen bound, rotated 90 degrees Z
         Instantiate(Border, new Vector3(screenBounds.x, 0, 0), Quaternion.Euler(0, 0, 90));//place border at right screen bound, rotated 90 degrees Z
-        Instantiate(Player5, new Vector3(0, -3, 0), transform.rotation);//create player, will become more complex when skin settings works
     }
 }
---
+++ Fishing Trip/Assets/Scripts/MenuScripts/SkinDropdown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Create a new Dropdown GameObject by going to the Hierarchy and clicking Create>UI>Dropdown. Attach this script to the Dropdown GameObject.
+//Set your own Text in the Inspector window
+public class SkinDropdown : MonoBehaviour
+{
+    private TMPro.TMP_Dropdown myDropdown;
+
+    void Start()
+    {
+        //Fetch the Dropdown GameObject
+        myDropdown = GetComponent<TMPro.TMP_Dropdown>();
+        //Add listener for when the dropdown is changed
+        myDropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(myDropdown);
+        });
+    }
+
+    //Ouput the new value of the Dropdown into Text
+    void DropdownValueChanged(TMPro.TMP_Dropdown change)
+    {
+        PlayerPrefs.SetInt("Skin", change.value);
+    }
+}
