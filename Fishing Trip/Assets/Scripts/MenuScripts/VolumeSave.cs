8854d9a33fec83aef317331c1afe6e1e1c8f7691
Alexander-Cole <71194937+Alexander-Cole@users.noreply.github.com>
2020-11-28 16:00:36 -0600

    Save SkinName and MusicVolume in PlayerPrefs.

---
+++ Fishing Trip/Assets/Scripts/MenuScripts/VolumeSave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSave : MonoBehaviour
+{
+
+    public Slider slider;
+    public float sliderValue;
+    public void ChangeSldier(float value)
+    {
+        sliderValue = value;
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("MusicVolume");
+    }
+}
